33813e719810ee9149d793b321c9e94f67f90a94
douglas <douglas.df1@gmail.com>
2023-03-01 12:36:11 -0300

    Exercicio 4

---
+++ Exercicios/Semana 5/ex1.js
@@ -0,0 +1,5 @@
+const mensagemOla = () =>{
+    console.log("Olá mundo");
+}
+
+mensagemOla();
---
+++ Exercicios/Semana 5/ex3.js
@@ -0,0 +1,9 @@
+let arrayNumeros = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+let arrayInvertido = [];
+
+arrayInvertido = (array) =>{
+    return array.reverse();
+}
+
+console.log(arrayNumeros);
+console.log(arrayInvertido(arrayNumeros));
