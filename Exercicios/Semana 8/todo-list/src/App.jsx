a3b72e3e51065b446600acceefc48cc7fdfd3213
douglas <douglas.df1@gmail.com>
2023-03-23 11:54:41 -0300

    ex3

--- Exercicios/Semana 8/todo-list/src/App.jsx
+++ Exercicios/Semana 8/todo-list/src/App.jsx
@@ -1,14 +1,15 @@
-import { useState } from 'react'
-import './App.css'
-import CardAdicionar from './components/CardAdicionar'
+import { useState } from "react";
+import "./App.css";
+import CardAdicionar from "./components/CardAdicionar";
 
 function App() {
+  const [tarefas, setTarefas] = useState([]);
 
   return (
     <div>
       <CardAdicionar />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
