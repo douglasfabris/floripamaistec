2e9bb030d8544c6423849833bfdf3c9e975321a0
douglas <douglas.df1@gmail.com>
2023-03-29 17:58:35 -0300

    Ex 3

--- Exercicios/Semana 9/m01s09-exercicios-todo-app/src/components/Header.jsx
+++ Exercicios/Semana 9/m01s09-exercicios-todo-app/src/components/Header.jsx
@@ -1,3 +1,5 @@
+import { Link } from "react-router-dom";
+
 export const Header = () => {
   return (
     <header className="header bg-body-secondary">
@@ -5,13 +7,13 @@ export const Header = () => {
         <span className="flex-grow-1">Tarefas.dev</span>
         <ul class="navbar-nav ">
           <li class="nav-item me-auto">
-            <a class="nav-link active" aria-current="page" href="#">
-              Home
+            <a class="nav-link active" aria-current="page">
+              <Link to="/home">Home</Link>
             </a>
           </li>
           <li class="nav-item">
-            <a class="nav-link" href="#">
-              Quem sou
+            <a class="nav-link">
+              <Link to="/quem-sou">Quem sou</Link>
             </a>
           </li>
         </ul>
