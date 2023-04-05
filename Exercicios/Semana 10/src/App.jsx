3d74b986aaee0efca16c462c1fd59cf7b2df7c20
douglas <douglas.df1@gmail.com>
2023-04-05 18:55:08 -0300

    Semana 10 ex 6

--- Exercicios/Semana 10/src/App.jsx
+++ Exercicios/Semana 10/src/App.jsx
@@ -1,9 +1,12 @@
+/* eslint-disable no-nested-ternary */
 import { useState } from "react"
 import DadosPessoaisisForm from "./components/DadosPessoais"
+import Endereco from "./components/Endereco"
+import Agradecimento from "./components/Agradecimento"
 
 
 function App() {
-  const [passoAtual, setPassoAtual] = useState(0)
+  const [passoAtual, setPassoAtual] = useState(2)
 
   function mudarPasso(passo, e) {
     e.preventDefault()
@@ -12,7 +15,10 @@ function App() {
 
   return (
     <div>
-      <DadosPessoaisisForm/>
+      <form>
+        { passoAtual === 0 ? (<DadosPessoaisisForm/>) : ( passoAtual ===1 ? (<Endereco/>) : (<Agradecimento/>))}
+      </form>
+      
     </div>
   )
 }
