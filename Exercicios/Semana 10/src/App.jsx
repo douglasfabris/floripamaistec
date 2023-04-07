494ae6b36b0a8c975b4c83daf8df1dcabd622425
douglas <douglas.df1@gmail.com>
2023-04-07 19:32:38 -0300

    Semana 10 ex7

--- Exercicios/Semana 10/src/App.jsx
+++ Exercicios/Semana 10/src/App.jsx
@@ -3,6 +3,7 @@ import { useState } from "react"
 import DadosPessoaisisForm from "./components/DadosPessoais"
 import Endereco from "./components/Endereco"
 import Agradecimento from "./components/Agradecimento"
+import { CSHeader } from "./SCHeader"
 
 
 function App() {
@@ -15,6 +16,10 @@ function App() {
 
   return (
     <div>
+      <CSHeader>
+        <h2>Cadastre-se gratuitamente!</h2>
+        <p>Faça o seu cadastro para ter acesso a todos os recursos da nossa plataforma</p>
+      </CSHeader>
       <form>
         { passoAtual === 0 ? (<DadosPessoaisisForm/>) : ( passoAtual ===1 ? (<Endereco/>) : (<Agradecimento/>))}
       </form>
---
+++ Exercicios/Semana 10/src/SCHeader.jsx
@@ -0,0 +1,7 @@
+/* eslint-disable import/prefer-default-export */
+import styled from "styled-components"
+
+export const CSHeader = styled.header`
+  color: green;
+  font-size: 16px;
+`;
