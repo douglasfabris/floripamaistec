6044c9c744fd06fa7434d163c69cddbec9483ff8
douglas <douglas.df1@gmail.com>
2023-05-19 11:01:34 -0300

    modulo 2 semana 2 ex3

--- Modulo 2/Exercicios/Semana 2/index.js
+++ Modulo 2/Exercicios/Semana 2/index.js
@@ -3,8 +3,15 @@ const fs = require("fs");
 
 http
   .createServer((req, res) => {
-    res.writeHead(200, { "Content-Type": "text/plain" });
-    res.write("Teste");
-    res.end();
+    switch (req.method) {
+      case "GET":
+        res.writeHead(200, { "Content-Type": "text/plain" });
+        res.write("Teste");
+        res.end();
+        break;
+      case "POST":
+        // lógica do POST
+        break;
+    }
   })
   .listen(3000);
