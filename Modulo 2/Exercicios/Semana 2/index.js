411111990340118ac484ec1f021671ffcfccfb2d
douglas <douglas.df1@gmail.com>
2023-05-20 14:45:26 -0300

    modulo 2 semana 2 ex4

--- Modulo 2/Exercicios/Semana 2/index.js
+++ Modulo 2/Exercicios/Semana 2/index.js
@@ -1,10 +1,22 @@
 const http = require("http");
 const fs = require("fs");
 
+function lerJson() {
+  fs.readFile("dados.json", (err, data) => {
+    if (err) {
+      console.log(err);
+      throw err;
+    }
+    const dados = JSON.parse(data);
+    return dados;
+  });
+}
+
 http
   .createServer((req, res) => {
     switch (req.method) {
       case "GET":
+        // Lógica do GET
         res.writeHead(200, { "Content-Type": "text/plain" });
         res.write("Teste");
         res.end();
