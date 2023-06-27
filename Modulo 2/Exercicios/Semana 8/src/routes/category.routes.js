7faabbe2c34e0352e557cd8110c282a9ab8c461a
douglas <douglas.df1@gmail.com>
2023-06-27 19:38:21 -0300

    exercicio 3 semana 8

---
+++ Modulo 2/Exercicios/Semana 8/src/routes/category.routes.js
@@ -0,0 +1,17 @@
+const {
+  createOneCategory,
+  listCategories,
+} = require("../controllers/category.controller");
+const { Router } = require("express");
+
+class CategoryRouter {
+  routesFromCategory() {
+    const categoryRoutes = Router();
+    categoryRoutes.post("/createOneCategory", createOneCategory);
+    categoryRoutes.get("/listCategories", listCategories);
+
+    return categoryRoutes;
+  }
+}
+
+module.exports = new CategoryRouter();
